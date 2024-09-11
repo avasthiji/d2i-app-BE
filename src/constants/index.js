458b82ae8c787f73cb3f6cf748a8c9a725670228
tushar-shukla-d2i <tusharshukla.d2i@gmail.com>
2024-09-11 18:52:18 +0530

    ResetPassword url minor fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const CONSTANTS = {
   LOGIN_URL: `${process.env.BASEURL}/login`,
-  INVITE_URL: `${process.env.BASEURLFE}/resetpassword?inviteCode=`,
+  INVITE_URL: `${process.env.BASEURLFE}/reset-password?inviteCode=`,
 };
 module.exports = CONSTANTS;
