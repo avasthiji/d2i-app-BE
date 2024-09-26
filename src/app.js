9b413ff2d4d29d47edc152acc8b7683f0f7e3930
tushar-shukla-d2i <tusharshukla.d2i@gmail.com>
2024-09-26 15:16:20 +0530

    Profile image upload path

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/uploads", express.static(process.env.UPLOAD_DIR));
 app.use("/api/v1", router);
 
 // catch 404 and forward to error handler
